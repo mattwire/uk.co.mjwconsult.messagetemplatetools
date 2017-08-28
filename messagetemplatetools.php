17d4ce95161328d28aa22a42666a88a4c2c21043
Matthew Wire <devel@mrwire.co.uk>
2017-08-28 21:48:42 +0100

    Initial import

---
+++ messagetemplatetools.php
@@ -0,0 +1,159 @@
+<?php
+
+require_once 'messagetemplatetools.civix.php';
+use CRM_Messagetemplatetools_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function messagetemplatetools_civicrm_config(&$config) {
+  _messagetemplatetools_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function messagetemplatetools_civicrm_xmlMenu(&$files) {
+  _messagetemplatetools_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function messagetemplatetools_civicrm_install() {
+  _messagetemplatetools_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function messagetemplatetools_civicrm_postInstall() {
+  _messagetemplatetools_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function messagetemplatetools_civicrm_uninstall() {
+  _messagetemplatetools_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function messagetemplatetools_civicrm_enable() {
+  _messagetemplatetools_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function messagetemplatetools_civicrm_disable() {
+  _messagetemplatetools_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function messagetemplatetools_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _messagetemplatetools_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function messagetemplatetools_civicrm_managed(&$entities) {
+  _messagetemplatetools_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function messagetemplatetools_civicrm_caseTypes(&$caseTypes) {
+  _messagetemplatetools_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function messagetemplatetools_civicrm_angularModules(&$angularModules) {
+  _messagetemplatetools_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function messagetemplatetools_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _messagetemplatetools_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+function messagetemplatetools_civicrm_pageRun(&$page) {
+  $pageName = $page->getVar('_name');
+  if ($pageName == 'CRM_Admin_Page_MessageTemplates') {
+    CRM_Core_Resources::singleton()->addScriptFile(E::LONG_NAME, 'js/previewButton.js');
+  }
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function messagetemplatetools_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function messagetemplatetools_civicrm_navigationMenu(&$menu) {
+  _messagetemplatetools_civix_insert_navigation_menu($menu, NULL, array(
+    'label' => E::ts('The Page'),
+    'name' => 'the_page',
+    'url' => 'civicrm/the-page',
+    'permission' => 'access CiviReport,access CiviContribute',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _messagetemplatetools_civix_navigationMenu($menu);
+} // */
---
+++ templates/CRM/Messagetemplatetools/Form/Preview.tpl
@@ -0,0 +1,90 @@
+{* HEADER *}
+
+<div class="crm-submit-buttons">
+  {include file="CRM/common/formButtons.tpl" location="top"}
+</div>
+
+{* FIELD EXAMPLE: OPTION 1 (AUTOMATIC LAYOUT) *}
+
+{foreach from=$elementNames item=elementName}
+  <div class="crm-section">
+    <div class="label">{$form.$elementName.label}</div>
+    <div class="content">{$form.$elementName.html}</div>
+    <div class="clear"></div>
+  </div>
+{/foreach}
+
+{if $tokens}
+  <div class="crm-section collapsed">
+    <div class="crm-accordion-header">{ts}Subject Tokens{/ts}</div>
+    <div class="crm-accordion-body collapsed">
+      <h3>Tokens</h3>
+      {foreach from=$tokens.tokens.subject key=k item=value}
+        {foreach from=$value item=v2}
+          {$k}.{$v2}<br />
+        {/foreach}
+      {/foreach}
+      <h3>Smarty</h3>
+      {foreach from=$tokens.smarty.subject item=value}
+        {$value}<br />
+      {/foreach}
+    </div>
+  </div>
+  <div class="crm-section collapsed">
+    <div class="crm-accordion-header">{ts}HTML Tokens{/ts}</div>
+    <div class="crm-accordion-body">
+      <h3>Tokens</h3>
+      {foreach from=$tokens.tokens.html key=k item=value}
+        {foreach from=$value item=v2}
+          {$k}.{$v2}<br />
+        {/foreach}
+      {/foreach}
+      <h3>Smarty</h3>
+      {foreach from=$tokenElements item=value}
+        {$form.$value.label} {$form.$value.html}<br />
+      {/foreach}
+    </div>
+  </div>
+  <div class="crm-section collapsed">
+    <div class="crm-accordion-header">{ts}Text Tokens{/ts}</div>
+    <div class="crm-accordion-body collapsed">
+      <h3>Tokens</h3>
+      {foreach from=$tokens.tokens.text key=k item=value}
+        {foreach from=$value item=v2}
+          {$k}.{$v2}<br />
+        {/foreach}
+      {/foreach}
+      <h3>Smarty</h3>
+      {foreach from=$tokens.smarty.text item=value}
+        {$value}<br />
+      {/foreach}
+    </div>
+  </div>
+{/if}
+
+{if $renderedMail}
+  <div class="crm-section">
+    <div class="crm-accordion-header">{ts}Subject{/ts}</div>
+    <div class="crm-accordion-body">{$renderedMail.subject}</div>
+  </div>
+  <div class="crm-section">
+    <div class="crm-accordion-header">{ts}HTML version{/ts}</div>
+    <div class="crm-accordion-body">{$renderedMail.html}</div>
+  </div>
+  <div class="crm-section">
+    <div class="crm-accordion-header">{ts}Text version{/ts}</div>
+    <div class="crm-accordion-body">{$renderedMail.text}</div>
+  </div>
+{/if}
+
+{* FIELD EXAMPLE: OPTION 2 (MANUAL LAYOUT)
+
+  <div>
+    <span>{$form.favorite_color.label}</span>
+    <span>{$form.favorite_color.html}</span>
+  </div>
+
+{* FOOTER *}
+<div class="crm-submit-buttons">
+  {include file="CRM/common/formButtons.tpl" location="bottom"}
+</div>
